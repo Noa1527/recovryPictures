27b4c20aaa7d29d7cb9a354543b6e5ab5e35c9fb
mickael <mickael@yaakadev.com>
2023-04-27 09:02:41 +0200

    [test]

--- read_image_from_sd_card.py
+++ read_image_from_sd_card.py
@@ -1,35 +1,8 @@
-# import cv2
-# import os
-
-# def read_image_from_sd_card(image_path):
-#     image = cv2.imread(image_path)
-#     return image
-
-# def display_image(image, window_name='Image'):
-#     cv2.imshow(window_name, image)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-
-# def is_image_file(file_name):
-#     lower_file_name = file_name.lower()
-#     return lower_file_name.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff'))
-
-# def main():
-#     sd_card_path = "/chemin/vers/votre/carte/sd"
-
-#     for file_name in os.listdir(sd_card_path):
-#         if is_image_file(file_name):
-#             image_path = os.path.join(sd_card_path, file_name)
-#             image = read_image_from_sd_card(image_path)
-#             display_image(image, window_name=f"Image: {file_name}")
-
-# if __name__ == '__main__':
-#     main()
 import os
 import cv2
 from PIL import Image
 import numpy as np
-
+import subprocess
 
 def is_image_corrupted(image_path):
     try:
@@ -59,8 +32,17 @@ def restore_image(image):
     restored_image = cv2.inpaint(image, mask.astype(np.uint8), 3, cv2.INPAINT_NS)
     return restored_image
 
+def recover_deleted_images(sd_card_path, output_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    photorec_executable = "./testdisk-7.2-WIP/photorec"  # Remplacez par le chemin réel de l'exécutable photorec
+    command = [photorec_executable, "/d", output_folder, "/cmd", sd_card_path, "search"]
+    subprocess.run(command)
+
 def main():
     sd_card_path = "/Volumes/NO NAME"
+    output_folder = "./RecoveredPictures"
 
     for file_name in os.listdir(sd_card_path):
         print(f"Traitement du fichier : {file_name}")
@@ -75,6 +57,9 @@ def main():
                 image = read_image_from_sd_card(image_path)
                 restored_image = restore_image(image)
                 display_image(restored_image, window_name=f"Restored Image: {file_name}")
+
+    recover_deleted_images(sd_card_path, output_folder)
+    print(f"Les images supprimées ont été récupérées dans le dossier : {output_folder}")
 
 if __name__ == '__main__':
     main()
